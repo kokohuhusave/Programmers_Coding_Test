d98a6cf49e2110c2e7cf74bf5513d063edf5a9b3
김준호 <92168240+kokohuhusave@users.noreply.github.com>
2024-11-08 10:57:57 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+
+    while (n >= a)
+    {
+        int coke = n / a * b;
+        n %= a;
+        answer += coke;
+
+        n += coke;
+    }
+    return answer;
+}
