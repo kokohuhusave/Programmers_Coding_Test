3d376375eae25c98fce394a27ce138006a77b17f
김준호 <92168240+kokohuhusave@users.noreply.github.com>
2025-05-19 23:21:36 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,65 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0}; // 상하좌우
+    static int[] dy = {0, 0, -1, 1};
+    static List<Integer> houseCounts;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        visited = new boolean[N][N];
+        houseCounts = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = line.charAt(j) - '0'; // 문자 '0' → 숫자 0
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    int count = dfs(i, j);
+                    houseCounts.add(count);
+                }
+            }
+        }
+
+        Collections.sort(houseCounts);
+        System.out.println(houseCounts.size());
+        for (int cnt : houseCounts) {
+            System.out.println(cnt);
+        }
+    }
+
+    static int dfs(int x, int y) {
+        visited[x][y] = true;
+        int count = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                if (map[nx][ny] == 1 && !visited[nx][ny]) {
+                    count += dfs(nx, ny);
+                }
+            }
+        }
+
+        return count;
+    }
+}
